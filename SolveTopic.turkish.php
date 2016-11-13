87ca50d4c41d505a4698d6e6e8136773b7560b5c
gevv <320voltmail@gmail.com>
2016-11-13 14:17:27 +0200

    Create SolveTopic.turkish.php

---
+++ SolveTopic.turkish.php
@@ -0,0 +1,30 @@
+<?php
+
+$txt['topic_solved_title'] = 'Konu Çözüldü';
+$txt['solve_topic'] = 'Konu Çözüldü';
+$txt['unsolve_topic'] = 'Konu Çözülmedi';
+
+$txt['modlog_ac_solve'] = '&quot;{topic}&quot; çözüldü olarak {member} tarafından işaretlendi';
+$txt['modlog_ac_unsolve'] = '&quot;{topic}&quot; çözülmedi olarak {member} tarafından işaretlendi';
+$txt['modlog_solve_log'] = 'Konu Çözüldü kayıtları';
+$txt['modlog_solve_log_desc'] = 'Aşağıda, çözülmüş veya çözülmemiş olarak işaretlenmiş konuların bir listesi bulunmaktadır.';
+$txt['modlog_solve_log_no_entries'] = 'Şu anda çözülmüş konu katdında hiç giriş yok.';
+$txt['modlog_solve_log_help'] = '<strong>Konu Çözüldü kayıtları</strong><br />Bu bölüm, moderatör ekibinin çözülen konu özelliğinin tüm kullanımını izlemesini sağlar.';
+$txt['modlog_solve_log_remove_selected_confirm'] = 'Seçilen kaydı tamamen silmek istediğinizden emin misiniz? \n24 saatten daha eski girişler silinmeyecek.';
+$txt['modlog_solve_log_remove_all_confirm'] = 'Kayıtları tamamen silmek istediğinizden emin misiniz? \n24 saatten daha eski girişler silinmeyecek.';
+
+$txt['permissionname_solve_topic'] = 'Konuları çözülmüş/çözülmemiş olarak işaretle';
+$txt['permissionhelp_solve_topic'] = 'Bu izin kullanıcıların konuları çözülmüş veya çözülmemiş olarak işaretlemelerine izin verir.';
+$txt['permissionname_solve_topic_any'] = 'Tüm konular';
+$txt['permissionname_solve_topic_own'] = 'Kendi konuları';
+$txt['permissionname_simple_solve_topic_own'] = 'Kendi konusunu çözüldü olarak işaretleyebilir';
+$txt['permissionname_simple_solve_topic_any'] = 'Tüm konuları çözüldü olarak işaretleyebilir';
+
+$txt['cannot_solve_topic_any'] = 'Konuları konuları çözüldü olarak işaretleme izniniz yok.';
+$txt['cannot_solve_topic_own'] = 'Konuları konuları çözüldü olarak işaretleme izniniz yok.';
+$txt['enable_solved_log'] = 'Çözülen konuların  kaydını etkinleştirin (Etkinleştirilmesi için forum kayıt günlüğünün açık olması gerekir)';
+
+$txt['solvetopic_board_desc'] = 'Lütfen konuyu çözüldü özelliğini etkinleştirmek istediğiniz bölümleri seçin.';
+$txt['solvetopic_display_notice'] = 'Çözülmüş konuların içinde çözüldüğüne dair not görüntüle';
+$txt['solvetopic_solved'] = '<strong>Konuyu Çözüldü/strong><br />Bu konu çözüldü olarak işaretlendi ve daha fazla dikkat gerektirir.';
+$txt['solvetopic_not_enabled'] = 'Konu çözüldü bu bölümde etkin değil.';
