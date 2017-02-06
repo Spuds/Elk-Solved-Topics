0bd94777036ce520c0569d00264b20e66d6ea0ea
spuds <spuds@spudsdesign.com>
2017-02-06 17:51:00 -0600

    ! add french translation

---
+++ SolveTopic.french.php
@@ -0,0 +1,30 @@
+<?php
+
+$txt['topic_solved_title'] = 'Sujet résolu';
+$txt['solve_topic'] = 'Sujet résolu';
+$txt['unsolve_topic'] = 'Marquer Non Résolu';
+
+$txt['modlog_ac_solve'] = 'Marqué Résolu &quot;{topic}&quot; par {member}';
+$txt['modlog_ac_unsolve'] = 'Marqué Non Résolu &quot;{topic}&quot; par {member}';
+$txt['modlog_solve_log'] = 'Journal des sujets résolus';
+$txt['modlog_solve_log_desc'] = 'Ci-dessous la liste des tous les sujets qui ont été marqués résolus ou non résolus.';
+$txt['modlog_solve_log_no_entries'] = 'Il y a actuellement aucune entrée dans le journal des sujets résolus.';
+$txt['modlog_solve_log_help'] = '<strong>Journal des sujets résolus</strong><br />Cette section permet à l\'équipe de modération de suivre toutes les activités de la rubrique résolue.';
+$txt['modlog_solve_log_remove_selected_confirm'] = 'Etes-vous sûr de vouloir supprimer les entrées sélectionnés dans le journal ? \nLes entrées de moins de 24 heures ne seront pas supprimées.';
+$txt['modlog_solve_log_remove_all_confirm'] = 'Etes-vous sûr de vouloir supprimer totalement le contenu du journal ? \nLes entrées de moins de 24 heures ne seront pas supprimées.';
+
+$txt['permissionname_solve_topic'] = 'Marquer le sujet résolu/non résolu';
+$txt['permissionhelp_solve_topic'] = 'Ce droit autorise l\'utilisateur à marquer un sujet résolu/non résolu.';
+$txt['permissionname_solve_topic_any'] = 'N\'importe quel sujet';
+$txt['permissionname_solve_topic_own'] = 'Votre sujet';
+$txt['permissionname_simple_solve_topic_own'] = 'Marquer vos propres sujets résolus';
+$txt['permissionname_simple_solve_topic_any'] = 'Marquer n\'importe quel sujet résolu';
+
+$txt['cannot_solve_topic_any'] = 'Vous n\'avez pas l\'autorisation de marquer ce sujet résolu.';
+$txt['cannot_solve_topic_own'] = 'Vous n\'avez pas l\'autorisation de marquer ce sujet résolu.';
+$txt['enable_solved_log'] = 'Activer la journalisation des sujets résolus (Nécessite que la modération de la journalisation soit aussi activée)';
+
+$txt['solvetopic_board_desc'] = 'Veuillez sélectionner la section où vous souhaitez activer la fonction Sujet Résolu.';
+$txt['solvetopic_display_notice'] = 'Afficher un sujet résolu dans des sujets résolus';
+$txt['solvetopic_solved'] = '<strong>Sujet résolu</strong><br />Ce sujet a été marqué résolu et ne nécessite aucune autre attention.';
+$txt['solvetopic_not_enabled'] = 'Sujet résolu n\'est pas activé dans cette section.';
