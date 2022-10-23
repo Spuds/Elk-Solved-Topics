5b77b484227fcd2714d33e551f4484d1c961b152
Spuds <spuds@spudsdesign.com>
2022-10-23 09:51:56 -0500

    ! Cleanup template so it displays normally

--- templates/SolveTopic.template.php
+++ templates/SolveTopic.template.php
@@ -2,7 +2,7 @@
 
 /**
  * @name      SolveTopic
- * @copyright 2014-2021 ElkArte Forum contributors
+ * @copyright 2014-2022 ElkArte Forum contributors
  * @license   BSD http://opensource.org/licenses/BSD-3-Clause
  *
  * This software is a derived product, based on:
@@ -10,7 +10,7 @@
  * TopicSolved 1.1.1
  * Copyright 2006-2008 Blue Dream (http://www.simpleportal.net)
  *
- * @version 1.0.2
+ * @version 1.0.3
  *
  */
 
@@ -34,36 +34,35 @@ function template_callback_selectboards()
 	// Since we are in a callback, close the config_vars DL we are currently in
 	echo '
 					</dl>
-					<ul class="ignoreboards">';
+					<dl>';
 
 	foreach ($context['categories'] as $category)
 	{
 		// Start with a category header
 		echo '
-						<li class="category">
-							<h3 class="strong success">', $category['name'], '</h3>
-							<ul>';
+						<dt class="category">
+							<div class="strong success">', $category['name'], '</div>
+							<hr />
+						</dt>';
 
 		// Every board in this category
 		foreach ($category['boards'] as $board)
 		{
 			// Board name checkbox
 			echo '
-								<li class="board" style="margin-', $context['right_to_left'] ? 'right' : 'left', ': ', $board['child_level'] * 2, 'em;">
-									<label for="solvetopic_board_', $board['id'], '">
-										<input type="checkbox" id="solvetopic_board_', $board['id'], '" name="solvetopic_board_', $board['id'], '" value="', $board['id'], '"', !empty($modSettings['solvetopic_board_' . $board['id']]) ? ' checked="checked"' : '', ' class="input_check" /> ', $board['name'], '
-									</label>
-								</li>';
+						<dd style="margin-', $context['right_to_left'] ? 'right' : 'left', ': ', $board['child_level'] * 2, 'em;">
+							<label for="solvetopic_board_', $board['id'], '">
+								<input type="checkbox" id="solvetopic_board_', $board['id'], '" name="solvetopic_board_', $board['id'], '" value="', $board['id'], '"', !empty($modSettings['solvetopic_board_' . $board['id']]) ? ' checked="checked"' : '', ' /> ', $board['name'], '
+							</label>
+						</dd>';
 		}
 
-		echo '
-							</ul>
-						</li>';
+		echo '<br />';
 	}
 
 	// Close our list and enter a Dl, just like we never left
 	echo '
-					</ul>
-					<br class="clear">
+					
+					</dl>
 					<dl class="settings">';
 }
